b270c0116bd9b625a27c9e65cf133661ea4d260b
smrafael <rafael.sm1990@gmail.com>
2017-09-24 13:39:42 -0300

    adding cluters, helmet, system log and self signed certs for ssl

--- libs/boot.js
+++ libs/boot.js
@@ -1,7 +1,16 @@
+import https from "https";
+import fs from "fs";
+
 module.exports = app => {
     app.db.sequelize.sync().done(() => {
         if (process.env.NODE_ENV !== "test") {
-            app.listen(app.get("port"), () => {
+
+            const credentials = {
+                key: fs.readFileSync("ntask.key", "utf8"),
+                cert: fs.readFileSync("ntask.cert", "utf8")
+            }
+
+            https.createServer(credentials, app).listen(app.get("port"), () => {
                 console.log(`NTask API - porta ${app.get("port")}`);
             });
         }
